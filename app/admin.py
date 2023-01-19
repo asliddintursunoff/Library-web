6872bd89e27c6037976258c09ecb3f9346402680
asliddintursunoff <asliddintursunoff@icloud.com>
2023-01-19 10:40:00 +0300

    pro

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
 from .models import Book
-admin.site.register(Book)
+
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['kitob_nomi',"rasm","kitob_pdf"]
+    list_filter = ('kitob_nomi',"time")
+    list_per_page = 10
+    search_fields = ['kitob_nomi']
+
+    class Meta:
+        model = Book
+
+
+admin.site.register(Book, BookAdmin)
